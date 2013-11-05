91898ad47cc839e900155a2b1ffa970c3b0ba88d
Jakub Bełcik <belcik.jakub@gmail.com>
2013-11-05 01:49:21 +0100

    zadanie 1c v1.2

---
+++ scripts/jbelcik/1c.js
@@ -0,0 +1,18 @@
+var conn = new Mongo();
+var db = conn.getDB('dataBase');
+var dataBase = db.train.find();
+var count = 0;
+
+dataBase.forEach(function (record) {
+	if (typeof record.tags === 'string') {
+		var tableOfTags = record.tags.split(' ');
+		count += 1;
+
+		db.train.update(
+			{_id: record._id},
+			{$set: {tags: tableOfTags}}
+		)
+	}
+});
+
+print(count + " updates");
