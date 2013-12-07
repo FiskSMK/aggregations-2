983d838bd4e47128f0601f9b155b08a2faf92cc2
Wlodek Bzyl <matwb@ug.edu.pl>
2013-12-07 20:30:22 +0100

    poprawki

--- scripts/wbzyl/tf_idf.js
+++ scripts/wbzyl/tf_idf.js
@@ -1,22 +1,23 @@
 
-db.tdidf.drop();
+db.tfidf.drop();
 
 var N = db.so.count();
 
 db.so.find().forEach(function (doc) {
   var freq = doc.freq;
   var max = doc.max;
-  doc.tdidf = {};
+  doc.tfidf = {};
 
   for (word in freq) { 
     var n = db.idf.find({_id: word}).count();
     // skip infrequent words
     if (n > 40) {
-      doc.tdidf[word] = freq[word]/max * Math.LOG2E * Math.log(N/n);;
+      doc.tfidf[word] = freq[word]/max * Math.LOG2E * Math.log(N/n);;
     }
   }
   delete doc.freq;
   delete doc.max;
   // printjson(doc);
-  db.tdidf.insert(doc);
+
+  db.tfidf.insert(doc);
 });
